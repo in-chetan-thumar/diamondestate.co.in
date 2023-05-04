3ba49e18b7aa5d54a33957b35d7d2c1ce8f50e58
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-05-04 16:45:29 +0530

    First commit

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Diamond Estate</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+      <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+      <link href="css/animate.css" rel="stylesheet" type="text/css">
+      <link href="css/jquery.fancybox.min.css" rel="stylesheet" type="text/css">
+      <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+      <link href="css/fonts.css" rel="stylesheet" type="text/css">
+      <link href="css/swiper.min.css" rel="stylesheet" type="text/css">
+      <link href="css/style.css" rel="stylesheet" type="text/css">
+      <link href="css/responsive.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+      <div  id="preloader" class="loader_show site-main">
+         <noscript>
+            <div class="noscript">
+               <p><i class="fa fa-exclamation-triangle"></i>Please enable JavaScript in your browser.</p>
+            </div>
+         </noscript>
+
+
+
+
+         <header class="header_main">
+               <div class="d-flex align-items-center justify-content-between">
+                  <div class="logo_main">
+                     <a href="index.php" title="Diamond Estate"><img src="images/diamondlogo.svg" alt=""></a>
+                  </div>
+                  <div class="menu_right">
+                     <a id="wsnavtoggle" class="wsanimated-arrow menu_close"><span></span><span></span><span></span><span></span></a>
+                     <nav class="wsmenu">
+                        <ul class="wsmenu-list">
+                           <li><a href="#aboutus" title="About us">About us</a></li>
+                           <li><a href="#projects" title="Projects">Projects</a></li>
+                           <li><a href="#contact" title="Contact">Contact</a></li>
+                        </ul>
+                     </nav>
+                  </div>
+               </div>
+         </header>
+
+
+
+
+
